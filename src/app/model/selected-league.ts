ea18784ed6a9176028c7a84cce76de2b9cc2fb55
GuillaumeVT <guillaume.von.tokarski@gmail.com>
2019-05-07 10:55:56 +0100

    Added league object in games

---
+++ src/app/model/selected-league.ts
@@ -0,0 +1,9 @@
+export class SelectedLeague {
+  id:           string;
+  createdBy:    string;
+  createdAt:    number;
+  updatedAt:    number;
+  name:         string;
+  kind:         string;
+  division:     string;
+}
